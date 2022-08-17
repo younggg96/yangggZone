241278dcc1d5c53c0cb1ee602a01a95711089f9c
Guanggenng Yang <42145357+younggg96@users.noreply.github.com>
2022-08-17 01:58:24 -0700

    🎉 add menu function

--- yanggg_playground_react_ts/src/App.tsx
+++ yanggg_playground_react_ts/src/App.tsx
@@ -4,73 +4,87 @@ import Button, {
   ButtonAppearance,
   ButtonVariant,
 } from "./components/Button/button";
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
 
 function App() {
   return (
     <div className="App">
-      <h1>Button</h1>
-      <h4>Variant</h4>
-      <div className="buttons">
-        <Button variant={ButtonVariant.Contained} color={ButtonColor.Info}>
-          Contained Button
-        </Button>
-        <Button variant={ButtonVariant.Outlined} color={ButtonColor.Info}>Outlined Button</Button>
-        <Button variant={ButtonVariant.Ghosted} color={ButtonColor.Info}>
-          Ghosted Button
-        </Button>
-      </div>
-      <h4>Colors</h4>
-      <div className="buttons">
-        <Button color={ButtonColor.Default}>Default Button</Button>
-        <Button color={ButtonColor.Primary}>Primary Button</Button>
-        <Button color={ButtonColor.Secondary}>Secondary Button</Button>
-        <Button color={ButtonColor.Info}>Info Button</Button>
-        <Button color={ButtonColor.Success}>Success Button</Button>
-        <Button color={ButtonColor.Danger}>Danger Button</Button>
-        <Button color={ButtonColor.Warning}>Warning Button</Button>
-      </div>
-      <h4>Sizes</h4>
-      <div className="buttons">
-        <Button size={ButtonSize.Small}>Small Button</Button>
-        <Button>Default Button</Button>
-        <Button size={ButtonSize.Large}>Large Button</Button>
-      </div>
-      <h4>Disabled</h4>
-      <div className="buttons">
-        <Button>Default Button</Button>
-        <Button disabled>Default Button</Button>
-      </div>
-      <h4>Links</h4>
-      <div className="buttons">
-        <Button link href="#">
-          Link
-        </Button>
-        <Button link href="#" disabled>
-          Disabled Link
-        </Button>
-      </div>
-      <h4>Appearance</h4>
-      <div className="buttons">
-        <span>Round:</span>
-        <Button size={ButtonSize.Small} appearance={ButtonAppearance.Round}>
-          Round Button
-        </Button>
-        <Button appearance={ButtonAppearance.Round}>Round Button</Button>
-        <Button size={ButtonSize.Large} appearance={ButtonAppearance.Round}>
-          Round Button
-        </Button>
-      </div>
-      <div className="buttons">
-        <span>Square:</span>
-        <Button appearance={ButtonAppearance.Square}>Square Button</Button>
-      </div>
-      <h4>Ripple</h4>
-      <div className="buttons">
-        <span>Default Ripple:</span>
-        <Button>No Ripple Button</Button>
-        <span>Disabled Ripple:</span>
-        <Button noRipple>No Ripple Button</Button>
-      </div>
+      <section>
+        <h1>Button</h1>
+        <h4>Variant</h4>
+        <div className="buttons">
+          <Button variant={ButtonVariant.Contained} color={ButtonColor.Info}>
+            Contained Button
+          </Button>
+          <Button variant={ButtonVariant.Outlined} color={ButtonColor.Info}>
+            Outlined Button
+          </Button>
+          <Button variant={ButtonVariant.Ghosted} color={ButtonColor.Info}>
+            Ghosted Button
+          </Button>
+        </div>
+        <h4>Colors</h4>
+        <div className="buttons">
+          <Button color={ButtonColor.Default}>Default Button</Button>
+          <Button color={ButtonColor.Primary}>Primary Button</Button>
+          <Button color={ButtonColor.Secondary}>Secondary Button</Button>
+          <Button color={ButtonColor.Info}>Info Button</Button>
+          <Button color={ButtonColor.Success}>Success Button</Button>
+          <Button color={ButtonColor.Danger}>Danger Button</Button>
+          <Button color={ButtonColor.Warning}>Warning Button</Button>
+        </div>
+        <h4>Sizes</h4>
+        <div className="buttons">
+          <Button size={ButtonSize.Small}>Small Button</Button>
+          <Button>Default Button</Button>
+          <Button size={ButtonSize.Large}>Large Button</Button>
+        </div>
+        <h4>Disabled</h4>
+        <div className="buttons">
+          <Button>Default Button</Button>
+          <Button disabled>Default Button</Button>
+        </div>
+        <h4>Links</h4>
+        <div className="buttons">
+          <Button link href="#">
+            Link
+          </Button>
+          <Button link href="#" disabled>
+            Disabled Link
+          </Button>
+        </div>
+        <h4>Appearance</h4>
+        <div className="buttons">
+          <span>Round:</span>
+          <Button size={ButtonSize.Small} appearance={ButtonAppearance.Round}>
+            Round Button
+          </Button>
+          <Button appearance={ButtonAppearance.Round}>Round Button</Button>
+          <Button size={ButtonSize.Large} appearance={ButtonAppearance.Round}>
+            Round Button
+          </Button>
+        </div>
+        <div className="buttons">
+          <span>Square:</span>
+          <Button appearance={ButtonAppearance.Square}>Square Button</Button>
+        </div>
+        <h4>Ripple</h4>
+        <div className="buttons">
+          <span>Default Ripple:</span>
+          <Button>No Ripple Button</Button>
+          <span>Disabled Ripple:</span>
+          <Button noRipple>No Ripple Button</Button>
+        </div>
+      </section>
+      <section>
+        <h1>Menu</h1>
+        <h4>Menu</h4>
+        <Menu defaultIndex={0} onSelect={(index) => {alert(index)}}>
+          <MenuItem index={0}>link 0 </MenuItem>
+          <MenuItem index={1}>link 1 </MenuItem>
+        </Menu>
+      </section>
     </div>
   );
 }
--- yanggg_playground_react_ts/src/components/Menu/menu.tsx
+++ yanggg_playground_react_ts/src/components/Menu/menu.tsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import classNames from "classnames";
 
 type MenuMode = "horizontal" | "vertical";
+type SelectCallback = (selectedIndex: number) => void;
+
 export interface MenuProps {
   defaultIndex?: number;
   className?: string;
   mode?: MenuMode;
   style?: React.CSSProperties;
-  children: React.ReactNode;
+  children?: React.ReactNode;
   onSelect?: (selectedIndex: number) => void;
 }
 
+interface IMenuContext {
+  index: number;
+  onSelect?: SelectCallback;
+}
+
+export const MenuContext = createContext<IMenuContext>({
+  index: 0,
+});
+
 const Menu: React.FC<MenuProps> = (props) => {
-  const { className, mode, style, children, defaultIndex } = props;
+  const { className, mode, style, children, defaultIndex, onSelect } = props;
+  const [activeIndex, setActiveIndex] = useState(defaultIndex);
   const classes = classNames("g-menu", className, {
     vertical: mode === "vertical",
   });
+  const handleClick = (index: number) => {
+    setActiveIndex(index);
+    if (onSelect) {
+      onSelect(index);
+    }
+  };
+  const passedContext: IMenuContext = {
+    index: activeIndex ? activeIndex : 0,
+    onSelect: handleClick,
+  };
   return (
     <ul className={classes} style={style}>
-      {children}
+      <MenuContext.Provider value={passedContext}>{children}</MenuContext.Provider>
     </ul>
   );
 };
